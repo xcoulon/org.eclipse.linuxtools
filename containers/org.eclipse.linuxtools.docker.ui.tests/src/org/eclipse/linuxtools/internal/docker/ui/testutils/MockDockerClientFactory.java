aeef282b3aebaa76fc8755480111c5cfdcad4b0b
Xavier Coulon <xcoulon@redhat.com>
2015-10-09 09:49:35 +0200

    bug 471666 - Show nested elements with most relevant info in Explorer View
    Showing ports, links and volumes for each container
    Loading ContainerInfo in background if it was not already
    loaded (hence, a new method in IDockerContainer)
    Make sure the 'volumes', 'ports' and 'links' nodes remain
     expanded when the 'refresh' command is called
    
    Added UI tests
    
    Change-Id: Id7b5f3982c660970824a8c8a564d49c9717fc0ce
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- containers/org.eclipse.linuxtools.docker.ui.tests/src/org/eclipse/linuxtools/internal/docker/ui/testutils/MockDockerClientFactory.java
+++ containers/org.eclipse.linuxtools.docker.ui.tests/src/org/eclipse/linuxtools/internal/docker/ui/testutils/MockDockerClientFactory.java
@@ -11,8 +11,8 @@
 
 package org.eclipse.linuxtools.internal.docker.ui.testutils;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import org.eclipse.linuxtools.docker.core.IDockerConnection;
@@ -21,6 +21,7 @@ import org.mockito.Mockito;
 import com.spotify.docker.client.DockerClient;
 import com.spotify.docker.client.DockerException;
 import com.spotify.docker.client.messages.Container;
+import com.spotify.docker.client.messages.ContainerInfo;
 import com.spotify.docker.client.messages.Image;
 
 /**
@@ -43,6 +44,8 @@ public class MockDockerClientFactory {
 		
 		private final DockerClient dockerClient;
 		
+		private final List<Container> containers = new ArrayList<>();
+		
 		private Builder() {
 			this.dockerClient = Mockito.mock(DockerClient.class);
 		}
@@ -56,25 +59,33 @@ public class MockDockerClientFactory {
 			return this;
 		}
 		
-		public Builder noImages() {
-			return images(Collections.emptyList());
-		}
 		
-		public DockerClient containers(final List<Container> containers) {
+		public Builder container(final Container container) {
+			this.containers.add(container);
+			return this;
+		}
+
+		public Builder container(final Container container, final ContainerInfo containerInfo)  {
+			this.containers.add(container);
 			try {
-				Mockito.when(dockerClient.listContainers(Mockito.any())).thenReturn(containers);
+				Mockito.when(this.dockerClient.inspectContainer(container.id())).thenReturn(containerInfo);
 			} catch (DockerException | InterruptedException e) {
 				// rest assured, nothing will happen while mocking the DockerClient
 			}
+			return this;
+		}
+		
+		public DockerClient build() {
+			try {
+				Mockito.when(this.dockerClient.listContainers(Mockito.any())).thenReturn(this.containers);
+			} catch (DockerException | InterruptedException e) {
+				// nothing may happen when mocking the method call 
+			}
 			return this.dockerClient;
 		}
-		
-		public DockerClient containers(final Container... containers) {
-			return containers(Arrays.asList(containers));
-		}
-		
+
 		public DockerClient noContainers() {
-			return containers(Collections.emptyList());
+			return this.dockerClient;
 		}
 		
 	}
